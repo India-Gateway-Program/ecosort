ed5212778adea69fc28791735b42cc5bba0efa07
Florian Jafar Sabonchi <sabonchi@posteo.de>
2024-12-18 23:34:47 +0100

    feat: first draft for BottomNavBar

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+// lib/colors.dart
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF0EB145);
+  static const Color secondaryColor = Colors.orange;
+  static const Color backgroundColor = Colors.white;
+  static const Color textColor = Colors.black;
+  static const Color unselectedItemColor = Colors.grey;
+}
---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,40 @@
+import 'package:ecosort/colors.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  const BottomNavBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.history),
+          label: 'History',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_month),
+          label: 'Calendar',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: 'Map',
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Assist'),
+      ],
+      currentIndex: selectedIndex,
+      selectedItemColor: AppColors.primaryColor,
+      unselectedItemColor: AppColors.unselectedItemColor,
+      onTap: onItemTapped,
+    );
+  }
+}
