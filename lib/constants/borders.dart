52341e18665b8fe1c4c9a32be447781104992c44
Florian Jafar Sabonchi <sabonchi@posteo.de>
2024-12-20 19:22:13 +0100

    feat: constants for border and colors added

---
+++ lib/constants/borders.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class AppBorders {
+  static BorderRadius small = BorderRadius.circular(5);
+  static BorderRadius medium = BorderRadius.circular(10);
+  static BorderRadius large = BorderRadius.circular(15);
+  static BorderRadius extraLarge = BorderRadius.circular(20);
+}
