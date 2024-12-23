99e4bbd6e1c815126f10e4cea9aa156fc9b01248
Florian Jafar Sabonchi <sabonchi@posteo.de>
2024-12-23 16:57:22 +0100

    feat: implemented takePhone in scan_screen.dart

--- lib/pages/scan_screen.dart
+++ lib/pages/scan_screen.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
-import 'package:camerawesome/camerawesome_plugin.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:camerawesome/camerawesome_plugin.dart';
+import 'package:flutter/scheduler.dart';
 
 class ScanScreen extends StatefulWidget {
   const ScanScreen({super.key});
@@ -10,12 +11,52 @@ class ScanScreen extends StatefulWidget {
 }
 
 class _ScanScreenState extends State<ScanScreen> {
+  CameraController? _cameraController;
+
   @override
   Widget build(BuildContext context) {
     return CameraAwesomeBuilder.custom(
+      enablePhysicalButton: true,
       builder: (CameraState state, Preview preview) {
-        return Row(
-          children: [],
+        return Stack(
+          children: [
+            Positioned(
+              bottom: 30,
+              left: 30,
+              right: 30,
+              child: ElevatedButton(
+                onPressed: () async {
+                  try {
+                    state.when(
+                      onPhotoMode: (photoCameraState) async {
+                        final file = await photoCameraState.takePhoto();
+                      },
+                    );
+                  } catch (e) {
+                    SchedulerBinding.instance.addPostFrameCallback((_) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(e.toString()),
+                          backgroundColor: Colors.red,
+                          duration: Duration(seconds: 3),
+                        ),
+                      );
+                    });
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(20),
+                  backgroundColor: Colors.white.withOpacity(0.7),
+                ),
+                child: const Icon(
+                  Icons.camera_alt,
+                  size: 40,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
         );
       },
       saveConfig: SaveConfig.photo(),
