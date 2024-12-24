f59cc170275707207e1264357b3e8add55bfc927
Florian Jafar Sabonchi <sabonchi@posteo.de>
2024-12-24 13:27:49 +0100

    feat: removed unused color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryColor = Color(0xFF0EB145);
-  static const Color secondaryColor = Colors.orange;
   static const Color backgroundColor = Colors.white;
   static const Color textColor = Colors.black;
   static const Color unselectedItemColor = Colors.grey;
